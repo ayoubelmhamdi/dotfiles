c31ba77c20d478a360115662efd18c2ab68ed10a
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2026-06-28 14:47:50 +0100

    add unactivate configs

---
+++ configs-files/zsh/void-sudo.zsh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+SUDO="sudo"
+if command -v doas >/dev/null 2>&1;then
+  SUDO="doas"
+fi
+
+alias fd="$SUDO fd"
+alias xbps-reconfigure="$SUDO xbps-reconfigure"
+alias vkpurge="$SUDO vkpurge"
+
+alias ntpdate="$SUDO ntpdate"
+alias date="$SUDO date"
+
+alias update-grub="$SUDO update-grub"
+alias reboot="$SUDO reboot"
+
+# mv, rm, cp
+alias rm="$SUDO rm -ivr"
+# alias rm="rm -Ivr"
+alias trash-list="trash-list --trash-dir=/root/.Trash-0"
+alias trash-restore="trash-restore --trash-dir=/root/.Trash-0"
+alias rmt="trash_rm"
+alias rmf="$SUDO /sbin/rm -vrf"
+
+
+alias chown="$SUDO chown"
+alias chmod="$SUDO chmod"
+alias x="$SUDO chmod +x"
+
+alias poweroff="$SUDO poweroff"
+alias reboot="$SUDO reboot"
+alias zzz="$SUDO zzz"
+alias pkill="$SUDO pkill"
+
+alias rmmod="$SUDO rmmod"
+alias modprobe="$SUDO modprobe"
+
+alias sv="$SUDO sv"
+
+alias mount="$SUDO mount"
+alias umount="$SUDO umount"
